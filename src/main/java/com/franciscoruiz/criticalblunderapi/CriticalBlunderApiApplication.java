408b6812c3ebd871cf24c3dc154033e94af466cd
Francisco <fruiz.alumno@iesrafaelalberti.es>
2022-12-06 17:01:27 +0100

    Initial Commit

---
+++ src/main/java/com/franciscoruiz/criticalblunderapi/CriticalBlunderApiApplication.java
@@ -0,0 +1,13 @@
+package com.franciscoruiz.criticalblunderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriticalBlunderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriticalBlunderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/franciscoruiz/criticalblunderapi/CriticalBlunderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciscoruiz.criticalblunderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriticalBlunderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
